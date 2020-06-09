64e6366b61a55ffa6263110987eb05b93a0a4972
Wiktoria <238039@student.pwr.edu.pl>
2020-06-09 22:50:11 +0200

    reformat

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,223 +1,218 @@
-import Head from 'next/head';
-import { ReactElement } from 'react';
-import { Button, Layout } from 'antd';
-const { Header, Footer, Content } = Layout;
-import { Colors } from '../constants/colors';
+import Head from 'next/head'
+import { ReactElement } from 'react'
+import { Button, Layout } from 'antd'
+const { Header, Footer, Content } = Layout
+import { Colors } from '../constants/colors'
 
 export default function Home(): ReactElement {
   return (
-        <div className="container">
-            <Layout className="layout" style={{ width: '100%' }}>
-                <Header
-                    style={{
-                      width: '100%',
-                      backgroundColor: Colors.primaryColor,
-                    }}
-                ></Header>
-                <Content
-                    style={{
-                      padding: '0 50px',
-                      backgroundColor: Colors.backgroundColor,
-                      paddingTop: '2%',
-                      maxHeight: 'auto',
-                    }}
-                >
-                    <div>
-                        <h1 className="title">Welcome to Spotify Lyrics!</h1>
-                        <p className="description">
-                            Find the lyrics of your favorite songs.
-                        </p>
-                        <Button
-                            type="primary"
-                            style={{
-                              backgroundColor: Colors.primaryColor,
-                              margin: 'auto',
-                              display: 'block',
-                              borderColor: Colors.primaryColor,
-                            }}
-                        >
-                            Sign in with Spotify
-                        </Button>
-                    </div>
-                    <img src="/walk.svg" alt="Music" className="music" />
-                </Content>
-                <Footer
-                    style={{
-                      textAlign: 'center',
-                      backgroundColor: Colors.footerColor,
-                    }}
-                >
-                    <a
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Powered by{' '}
-                        <img
-                            src="/spotify.svg"
-                            alt="Vercel Logo"
-                            className="logo"
-                        />
-                    </a>
-                </Footer>
-            </Layout>
+    <div className="container">
+      <Layout className="layout" style={{ width: '100%' }}>
+        <Header
+          style={{
+            width: '100%',
+            backgroundColor: Colors.primaryColor,
+          }}
+        ></Header>
+        <Content
+          style={{
+            padding: '0 50px',
+            backgroundColor: Colors.backgroundColor,
+            paddingTop: '2%',
+            maxHeight: 'auto',
+          }}
+        >
+          <div>
+            <h1 className="title">Welcome to Spotify Lyrics!</h1>
+            <p className="description">
+              Find the lyrics of your favorite songs.
+            </p>
+            <Button
+              type="primary"
+              style={{
+                backgroundColor: Colors.primaryColor,
+                margin: 'auto',
+                display: 'block',
+                borderColor: Colors.primaryColor,
+              }}
+            >
+              Sign in with Spotify
+            </Button>
+          </div>
+          <img src="/walk.svg" alt="Music" className="music" />
+        </Content>
+        <Footer
+          style={{
+            textAlign: 'center',
+            backgroundColor: Colors.footerColor,
+          }}
+        >
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/spotify.svg" alt="Vercel Logo" className="logo" />
+          </a>
+        </Footer>
+      </Layout>
 
-            <style jsx>{`
-                .container {
-                    min-height: 100vh;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                }
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-                main {
-                    padding: 5rem 0;
-                    flex: 1;
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: center;
-                    align-items: center;
-                }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-                footer {
-                    width: 100%;
-                    height: 100px;
-                    border-top: 1px solid #eaeaea;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-                footer img {
-                    margin-left: 0.5rem;
-                }
+        footer img {
+          margin-left: 0.5rem;
+        }
 
-                footer a {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
 
-                a {
-                    color: inherit;
-                    text-decoration: none;
-                }
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
 
-                .title a {
-                    color: #0070f3;
-                    text-decoration: none;
-                }
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
 
-                .title a:hover,
-                .title a:focus,
-                .title a:active {
-                    text-decoration: underline;
-                }
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
 
-                .title {
-                    margin: 0;
-                    line-height: 1.15;
-                    font-size: 4rem;
-                }
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
 
-                .title,
-                .description {
-                    text-align: center;
-                    color: white;
-                    text-shadow: 1px 1px 3px rgb(153, 153, 153);
-                }
+        .title,
+        .description {
+          text-align: center;
+          color: white;
+          text-shadow: 1px 1px 3px rgb(153, 153, 153);
+        }
 
-                .description {
-                    line-height: 1.5;
-                    font-size: 1.5rem;
-                }
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
 
-                code {
-                    background: #fafafa;
-                    border-radius: 5px;
-                    padding: 0.75rem;
-                    font-size: 1.1rem;
-                    font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-                        DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New,
-                        monospace;
-                }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+        }
 
-                .grid {
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    flex-wrap: wrap;
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
 
-                    max-width: 800px;
-                    margin-top: 3rem;
-                }
+          max-width: 800px;
+          margin-top: 3rem;
+        }
 
-                .music {
-                    margin: auto;
-                    display: block;
-                    width: 70%;
-                    height: 70%;
-                    padding-top: 5%;
-                    margin-bottom: -25%;
-                }
+        .music {
+          margin: auto;
+          display: block;
+          width: 70%;
+          height: 70%;
+          padding-top: 5%;
+          margin-bottom: -25%;
+        }
 
-                .card {
-                    margin: 1rem;
-                    flex-basis: 45%;
-                    padding: 1.5rem;
-                    text-align: left;
-                    color: inherit;
-                    text-decoration: none;
-                    border: 1px solid #eaeaea;
-                    border-radius: 10px;
-                    transition: color 0.15s ease, border-color 0.15s ease;
-                }
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
 
-                .card:hover,
-                .card:focus,
-                .card:active {
-                    color: #0070f3;
-                    border-color: #0070f3;
-                }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
 
-                .card h3 {
-                    margin: 0 0 1rem 0;
-                    font-size: 1.5rem;
-                }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
 
-                .card p {
-                    margin: 0;
-                    font-size: 1.25rem;
-                    line-height: 1.5;
-                }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
 
-                .logo {
-                    height: 1em;
-                }
+        .logo {
+          height: 1em;
+        }
 
-                @media (max-width: 600px) {
-                    .grid {
-                        width: 100%;
-                        flex-direction: column;
-                    }
-                }
-            `}</style>
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
 
-            <style jsx global>{`
-                html,
-                body {
-                    padding: 0;
-                    margin: 0;
-                    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,
-                        Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-                        Helvetica Neue, sans-serif;
-                }
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
 
-                * {
-                    box-sizing: border-box;
-                }
-            `}</style>
-        </div>
-    );
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
 }
