b57ac20355eaea9c9cb0a1c9d8ad30328dbcb7e5
donniebrighto <damian.wawrzyniak97@gmail.com>
2020-06-13 15:04:54 +0200

    move from openapi 2 to openapi 3

--- backend/src/main/java/pwr/piisw/backend/exceptions/GeniusAuthorizationException.java
+++ backend/src/main/java/pwr/piisw/backend/exceptions/GeniusAuthorizationException.java
@@ -3,7 +3,7 @@ package pwr.piisw.backend.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.SERVICE_UNAVAILABLE, reason = "Cannot authenticate to lyrics provider")
+@ResponseStatus(code = HttpStatus.SERVICE_UNAVAILABLE, reason = "Cannot authenticate to Genius")
 public class GeniusAuthorizationException extends RuntimeException {
 
     public GeniusAuthorizationException(String message, Throwable cause) {
