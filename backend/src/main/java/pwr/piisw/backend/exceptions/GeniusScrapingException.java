afa42bbe08b899f0c4f9f07ce71d32c09aeda79c
donniebrighto <damian.wawrzyniak97@gmail.com>
2020-06-13 14:10:08 +0200

    add ResponseStatus to custom exceptions

--- backend/src/main/java/pwr/piisw/backend/exceptions/GeniusScrapingException.java
+++ backend/src/main/java/pwr/piisw/backend/exceptions/GeniusScrapingException.java
@@ -1,5 +1,9 @@
 package pwr.piisw.backend.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Exception during Genius scraping")
 public class GeniusScrapingException extends RuntimeException {
 
     public GeniusScrapingException(String message) {
--- backend/src/main/java/pwr/piisw/backend/exceptions/SongNotFoundException.java
+++ backend/src/main/java/pwr/piisw/backend/exceptions/SongNotFoundException.java
@@ -1,5 +1,9 @@
 package pwr.piisw.backend.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Song not found")
 public class SongNotFoundException extends RuntimeException {
 
     public SongNotFoundException(String message) {
